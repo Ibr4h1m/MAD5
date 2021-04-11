c128fac28ffe59c41786929688e362dd34ff24a7
Ibrahim Abdul <ibrahimabdulb@outlook.com>
2021-04-11 01:55:39 +0100

    PWA Assignment
    MAD5 Assignment 3 Complete - Installable PWA

--- js/main.js
+++ js/main.js
@@ -1,85 +1,101 @@
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-        .then(function(){
-            console.log("SW Registered")
+
+//check whether browser support service workers
+if ('serviceWorker' in navigator) {
+    //wait until page loaded to avoid delaying rendering
+    window.addEventListener('load', function() {
+        //register service worker
+        navigator.serviceWorker.register('sw.js').then(
+            function(registration) {
+                console.log('Service worker registration successful', 
+                             registration);
+            }, 
+            function(err) {
+                console.log('Service worker registration failed', err);
+        });
     });
 }
 
+
+
+/*
+
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  const london = { lat: 51.509865, lng: -0.118092 };
-  const manchester = { lat: 53.483959, lng: -2.244644 };
-  const cambridge = { lat: 52.205276, lng: 0.119167 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: london,
-  });
-  // The marker, positioned at Uluru
-  const uluruMarker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-    
-  const londonMarker = new google.maps.Marker({
-    position: london,
-    map: map,
-  });
-    
-  const manchesterMarker = new google.maps.Marker({
-    position: manchester,
-    map: map,
-  });
-    
-  const cambridgeMarker = new google.maps.Marker({
-    position: cambridge,
-    map: map,
-  });
-    
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    const london = { lat: 51.509865, lng: -0.118092 };
+    const manchester = { lat: 53.483959, lng: -2.244644 };
+    const cambridge = { lat: 52.205276, lng: 0.119167 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: london,
+    });
+    // The marker, positioned at Uluru
+    const uluruMarker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+
+    const londonMarker = new google.maps.Marker({
+        position: london,
+        map: map,
+    });
+
+    const manchesterMarker = new google.maps.Marker({
+        position: manchester,
+        map: map,
+    });
+
+    const cambridgeMarker = new google.maps.Marker({
+        position: cambridge,
+        map: map,
+    });
+
 }
 
 
 function displayMessage(){
     var box = document.getElementById('welcome-box');
     box.innerHTML = '';
-    
+
     var name = document.getElementById('userName').value;
-    
+
     var getName = document.createTextNode(
-        "Welcome " + 
-        name + 
+        "Welcome " +
+        name +
         " Please report any bugs at www.1k4home.com. Thank you! Ibrahim Abdul | Staffordshire University | " );
     box.appendChild(getName);
 };
 
 function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  document.querySelector('.status').innerHTML = "Sending...";
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.querySelector('.status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.querySelector('.status').innerHTML = "Sending...";
 }
+
+*/
--- sw.js
+++ sw.js
@@ -1,37 +1,74 @@
-self.addEventListener('install', function(event){
-    console.log("SW Installed");
+const CACHE_NAME = 'tourguide-site';
+const CACHE_URLS =  ['index.html',
+    'help.html',
+    'destinations.html',
+    'contact.html',
+    'js/main.js',
+    'css/style.css',
+    'sw.js'
+];
+
+
+/*
+                     'images/cambridge-1.png',
+                     'images/cambridge-2.png',
+                     'images/logo.png',
+                     'images/london-1.png',
+                     'images/london-2.png',
+                     'images/manchester-1.png',
+                     'images/manchester-2.png',
+                     'images/me.png',
+*/
+
+//set up cache and files to add to it
+//...
+
+//add all URLs to cache when installed
+self.addEventListener("install", function(event){
+    console.log("Service worker installed");
     event.waitUntil(
-            caches.open('static')
+        //create and open cache
+        caches.open(CACHE_NAME)
             .then(function(cache){
-            /*cache.add('/');
-            cache.add('/index.html');
-            cache.add('/js/main.js');*/
-            cache.addAll([
-                '/',
-                '/index.html',
-                '/contact.html',
-                '/destinations.html',
-                '/help.html',
-                '/js/main.js',
-                '/css/style.css'
-            ]);
+                console.log("Cache opened");
+                //add all URLs to cache
+                return cache.addAll(CACHE_URLS);
+            })
+    );
+});
+
+//On activate update the cache with the new version and clean out old caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName.startsWith('tourguide-site') && CACHE_NAME !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
-self.addEventListener('activate', function(event){
-    console.log("SW Activated");
+
+//add all URLs to cache when installed
+//...
+//user has navigated to page - fetch required assets
+self.addEventListener("fetch", function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            //check whether asset is in cache
+            if(response){
+                //asset in cache, so return it
+                console.log(`Return ${event.request.url} from cache`);
+                return response;
+            }
+            //asset not in cache so fetch asset from network
+            console.log(`Fetch ${event.request.url} from network`);
+            return fetch(event.request);
+        })
+    );
 });
 
-/*self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(res){
-            if (res){
-                return res;
-            } else{
-                return fetch(event.request);
-            }
-        })
-    )
-})*/
