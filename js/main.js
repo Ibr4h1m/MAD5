e45ff1be26667c627597b15cef390db823c6be3b
Ibrahim Abdul <ibrahimabdulb@outlook.com>
2021-04-13 19:24:25 +0100

    New Build
    - Features added.
    - Content added.
    - Checklist added.
    - Fixed form.
    - Dark & Light mode added.

--- js/main.js
+++ js/main.js
@@ -59,12 +59,12 @@ function displayMessage(){
 function validateForm() {
     var name =  document.getElementById('name').value;
     if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        document.querySelector('.status').innerHTML = "Please enter your name!";
         return false;
     }
     var email =  document.getElementById('email').value;
     if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        document.querySelector('.status').innerHTML = "Please enter your email";
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -75,13 +75,13 @@ function validateForm() {
     }
     var subject =  document.getElementById('subject').value;
     if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        document.querySelector('.status').innerHTML = "Please enter the subject";
         return false;
     }
     var message =  document.getElementById('message').value;
     if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        document.querySelector('.status').innerHTML = "Please enter your message";
         return false;
     }
-    document.querySelector('.status').innerHTML = "Sending...";
+    document.querySelector('.status').innerHTML = "Email sent!";
 }
